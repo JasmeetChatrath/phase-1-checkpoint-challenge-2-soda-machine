ad3a080b2ad5014ecca0583924bcda4224171cc8
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-08-29 14:50:51 -0700

    Add attempt for find_soda and sell

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    if @sodas.include?(soda_brand)
+      return soda.brand
+    else
+      return nil
+    end
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand) == soda.brand
+      @sodas.delete(soda_brand)
+      @cash += soda.price
+    else
+      return nil
+    end
   end
-
 end
